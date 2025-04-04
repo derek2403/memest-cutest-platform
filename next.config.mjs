f4b89146b7c4d603c1c7402d63c5f2cee168df60
Avoisavo <lvera296@gmail.com>
2025-04-05 05:27:03 +0800

    debu

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Add configuration to disable SWC binary download for incompatible platforms
+  swcMinify: true,
 };
 
 export default nextConfig;
