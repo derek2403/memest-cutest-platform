37efdaa21027f19aa95187f387abfa10c92a5732
JingYuan <jingyuan0926@gmail.com>
2025-04-04 23:46:53 +0800

    Update tailwindcss to v3.4.17

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 import Header from '../components/Header';
-
+import {Button} from "@heroui/button";
 
 
 export default function Home() {
   return (
+    <div>
       <Header />
+      <Button color="primary">Button</Button>
+    </div>
   );
 }
